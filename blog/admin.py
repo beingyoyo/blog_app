27ab4f094f7fe06eea6b7c996919353a4ff9aae3
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2019-12-14 20:04:46 +0530

    First Commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from django.db import models
 from .models import Blog
+from tinymce.widgets import TinyMCE
 # Register your models here.
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Title / Date", {'fields' : ['blog_title', 'blog_published']}),
+        ("Content", {"fields" : ["blog_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField : {'widget' : TinyMCE()}
+    }
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class Blog(models.Model):
     blog_title = models.CharField(max_length=100)
     blog_content = models.TextField()
-    blog_published = models.DateTimeField('Date Published')
+    blog_published = models.DateTimeField('Date Published', default=datetime.now)
 
     def __str__(self):
         return self.blog_title
