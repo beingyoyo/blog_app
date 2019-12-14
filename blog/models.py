d356a929a8e1944b6f5dd3003e9062abee0cd765
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2019-12-14 19:49:50 +0530

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=100)
+    blog_content = models.TextField()
+    blog_published = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.blog_title
